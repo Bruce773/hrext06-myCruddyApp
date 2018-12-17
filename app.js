d2ae8fed07c5642736e60d5d12ea0a5b3ff915c7
Bruce Johnson <bruce@yogainternational.com>
2018-12-17 13:50:32 -0500

    added second input for title

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
   console.log('jQuery loaded');
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
+    localStorage.setItem('inputFieldValue', $('.text-entry-content').val());
     var myItemInStorage = localStorage.getItem('inputFieldValue');
     console.log('myItemInStorage', myItemInStorage);
 
