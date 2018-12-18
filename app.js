a88b1a553190a9653924f747a141d477e1ef1563
Bruce Johnson <bruce@yogainternational.com>
2018-12-18 09:37:33 -0500

    added styling and note creation functionality

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){ 
   console.log('jQuery loaded');
 
+  //iterate over localStorage, clear DOM, and append all notes in correct format 
+
   // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
+  $('.btn-submit').on('click','button', function(){
     localStorage.setItem($('.text-entry-title').val(), $('.text-entry-content').val());
     var myItemInStorage = localStorage.getItem($('.text-entry-title').val());
-    console.log($('.text-entry-title').val(), myItemInStorage);
+    // console.log($('.text-entry-title').val(), myItemInStorage);
 
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+    // create a div class = 'note'
+    var noteDiv = $('<div class="note"></div>');
+    //append title
+    var title = $('<span class="title"></span>').text($('.text-entry-title').val());
+    noteDiv.append(title);
+    //append content
+    noteDiv.append(myItemInStorage);
+    //append delete
+    noteDiv.append('<button class="delete-this-note">Delete Note</button>');
+    // append div to $('.list-display-field')
+    $('.list-display-field').append(noteDiv);
+
+    // $('.list-display-field').text(myItemInStorage); // ??
 
   });
 
