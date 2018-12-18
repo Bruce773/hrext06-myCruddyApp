a2bddcc340cdba461b743dbf479845ec8156f125
Bruce Johnson <bruce@yogainternational.com>
2018-12-18 13:12:29 -0500

    preparing to add delete button functionality -added ids to notes -began researching jquery id and class comparators

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   console.log("jQuery loaded");
   // console.log(localStorage.key(0));
+  var idCounter = 0;
   var appendNoteDiv = function(title, content) {
     // console.log($('.text-entry-title').val(), myItemInStorage);
     // create a div class = 'note'
-    var noteDiv = $('<div class="note"></div>');
+    idCounter++;
+    var noteDiv = $(`<div id=${idCounter} class='note'></div>`);
     //append title
-    var title = $('<span class="title"></span>').text(title);
+    var title = $(`<span id=${idCounter} class="title"></span>`).text(title);
     noteDiv.append(title);
     //append content
     noteDiv.append(content);
     //append delete
     noteDiv.append(
-      '<div class="delete-this-note"><button class="delete-this-note">Delete Note</button></div>'
+      `<div id=${idCounter} class="delete-this-note"><button id=${idCounter} class="delete-this-note">Delete Note</button></div>`
     );
     // append div to $('.list-display-field')
     $(".list-display-field").append(noteDiv);
@@ -25,23 +27,26 @@ $(document).ready(function() {
       // console.log(localStorage.key(i));
       var currentTitle = localStorage.key(i);
       var currentContent = localStorage.getItem(currentTitle);
-      console.log(currentTitle, currentContent);
+      // console.log(currentTitle, currentContent);
       appendNoteDiv(currentTitle, currentContent);
     }
   };
-  var autoRepopulateFunc = function(){
+  var autoRepopulateFunc = function() {
     if (localStorage.length === 0) {
       //if localStorage is empty
-      $(".list-display-field").html("<p>You haven't created any notes yet.</p>");
+      $(".list-display-field").html(
+        "<p>You haven't created any notes yet.</p>"
+      );
     } else {
       repopulateNotesOnPage();
     }
-  }
+  };
   autoRepopulateFunc();
   // write to local storage from input when button save clicked
   $(".btn-submit").on("click", "button", function() {
     // console.log($('.text-entry-title').val());
-    if ($(".text-entry-title").val() !== "") { //if the title input is not blank
+    if ($(".text-entry-title").val() !== "") {
+      //if the title input is not blank
       //if the title already exists and the content input is not blank
       //set the value of the existing title to the new content
 
@@ -49,7 +54,7 @@ $(document).ready(function() {
         $(".text-entry-title").val(),
         $(".text-entry-content").val()
       );
-      $('.list-display-field').html(' ')  
+      $(".list-display-field").html(" ");
       repopulateNotesOnPage();
 
       // var myItemInStorage = localStorage.getItem($(".text-entry-title").val());
@@ -57,8 +62,9 @@ $(document).ready(function() {
     }
   });
   // delete from local storage when delete button clicked
-  $(".btn-delete").on("click", function() {
-    localStorage.removeItem($(".text-entry-title").val());
+  $(".delete-this-note").on("click", "button", function() {
+    // console.log($(this).attr('class')); // get the attribute of the current button
+    console.log($(this).attr("id"));
   });
 
   $(".btn-delete-all").on("click", ".btn-delete-all", function() {
@@ -66,7 +72,7 @@ $(document).ready(function() {
       "Are you sure you want to delete ALL your notes? This action can NOT be undone!"
     );
     localStorage.clear();
-    $('.list-display-field').html(' ')  
+    $(".list-display-field").html(" ");
     autoRepopulateFunc();
   });
 });
